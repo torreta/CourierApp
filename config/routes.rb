865b07fbf36d0c68278c8ffba15a44b48780e843
Luis Campos <luiscampostorreta@gmail.com>
2014-09-26 21:17:57 +0000

    coloque welcome index como la pagina de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
-
-  get 'welcome/pricing'
-
-  get 'welcome/people'
-
   resources :agencies
 
-  root to: 'agencies#index'
+  root to: 'welcome#index'
 end
