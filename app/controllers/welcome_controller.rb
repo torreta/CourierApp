a19e1d39a6166e3d60311b171d345e00e7abf4ce
Luis Campos <luiscampostorreta@gmail.com>
2014-09-26 21:15:36 +0000

    colocando las nuevas vistas y rutas al navbar

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def pricing
+  end
+
+  def people
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/pricing'
+
+  get 'welcome/people'
+
   resources :agencies
 
   root to: 'agencies#index'
