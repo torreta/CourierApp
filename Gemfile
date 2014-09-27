4dc1e833896515e17b47f85296a8d322e0a07cc6
Luis Campos <luiscampostorreta@gmail.com>
2014-09-27 04:09:06 +0000

    Devise colocado sin el servicio de mailer pero operando... incluso links a perfil y log out

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'spring',        group: :development
 
 gem 'bootstrap-sass','~>2.3.1.0'
 
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,12 @@
+#este archivo lo dejo vacio por ahora, lo mas probable es que lo necesite para configurar el mailer
+
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#  :address              =>       'smtp.sendgrid.net ',
+#  :port                 =>       '587',
+#  :authentication       =>       :plain,
+#  :user_name            =>       'XD adivina', #realmente nose
+#  :password             =>       ' 1234   ',  # un peo, aqui se supone que van las credenciales de sendgrid, intentare poner las de gmail
+#  :domain               =>       'heroku.com'
+#  :enablestarttls_auto  =>       true
+# }
