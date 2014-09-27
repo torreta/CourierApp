968ab02215049db26d0631a7d26eaac526863589
Luis Campos <luiscampostorreta@gmail.com>
2014-09-27 17:51:41 +0000

    alfin paso parametros a la vista profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,13 @@
 class ProfileController < ApplicationController
   def edit
+    #@user = User.find(params[:id])
+   # @user = User.find(profile_params)
+  #rescue
+  #  @user = User.find(id=2)
+  end
+  
+  
+  def profile_params
+    params.require(:user).permit(:id)
   end
 end
